3c946ed53e6d176612f57d6fa6bbb50924c721a7
lia15 <lia@extramus.eu>
2022-08-11 17:04:34 -1200

    front end first commit

---
+++ pages/InterviewScheduled/Scheduled.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Calendar() {
+    return (
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FRome&mode=WEEK&hl=en_GB&src=am40Nzdpb2llNjJzanBpbXZjZHQ4dnRvMWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%237CB342" style={{border:"1px #777", width:"1230px", height:"620px", frameborder:"0", scrolling:"yes",margin:"10"}}></iframe>
+        )
+}
