17a1306c3f94262eb4d397630256579ff906a0f9
sinemturkcu <sinemturkcu@gmail.com>
2022-08-29 17:19:50 +0200

    link document list to db

--- components/DocumentList/DocumentList.jsx
+++ components/DocumentList/DocumentList.jsx
@@ -1,117 +1,159 @@
 import {
-	ArrowRightAlt,
-	AssignmentTurnedInOutlined,
-	CheckCircleOutline,
-	MoreHoriz,
-	WorkOutline,
+  ArrowRightAlt,
+  AssignmentTurnedInOutlined,
+  CheckCircleOutline,
+  MoreHoriz,
+  WorkOutline,
 } from "@mui/icons-material";
+import { Circle } from "@mui/icons-material";
+import { useEffect, useState } from "react";
 
 const DocumentListContent = ({ title, pos, status }) => {
-	const Border = () => {
-		let isRounded;
-		let statusColor;
+  const Border = () => {
+    let isRounded;
+    let statusColor;
 
-		pos === "left"
-			? (isRounded = " rounded-l-md ")
-			: pos === "right"
-			? (isRounded = " rounded-r-md ")
-			: isRounded;
+    pos === "left"
+      ? (isRounded = " rounded-l-md ")
+      : pos === "right"
+      ? (isRounded = " rounded-r-md ")
+      : isRounded;
 
-		status === "correct"
-			? (statusColor = " bg-green-400 ")
-			: status === "incorrect"
-			? (statusColor = " bg-red-400 ")
-			: status === "review"
-			? (statusColor = " bg-blue-400 ")
-			: (statusColor = " bg-gray-400 ");
+    status === "correct"
+      ? (statusColor = " bg-green-400 ")
+      : status === "incorrect"
+      ? (statusColor = " bg-red-400 ")
+      : status === "review"
+      ? (statusColor = " bg-blue-400 ")
+      : (statusColor = " bg-gray-400 ");
 
-		let result =
-			"flex flex-col items-center px-2 py-1 w-full gap-1 text-white " +
-			isRounded +
-			statusColor;
-		return result;
-	};
+    let result =
+      "flex flex-col items-center px-2 py-1 w-full gap-1 text-white " +
+      isRounded +
+      statusColor;
+    return result;
+  };
 
-	return (
-		<div className={Border()}>
-			<div className="text-[10px] ">{title}</div>
-			<CheckCircleOutline className="text-sm" />
-		</div>
-	);
+  return (
+    <div className={Border()}>
+      <div className="text-[10px] ">{title}</div>
+      <CheckCircleOutline className="text-sm" />
+    </div>
+  );
 };
 
 const Contents = [
-	{ title: "Curiculum Vitae", pos: "left", status: "correct" },
-	{ title: "Learning Agreement", pos: "mid", status: "correct" },
-	{ title: "Invitation Letter", pos: "mid", status: "review" },
-	{ title: "Accomodation Letter", pos: "mid", status: "null" },
-	{ title: "Arrival Tickets", pos: "mid", status: "null" },
-	{ title: "Intern Dev Plan", pos: "mid", status: "incorrect" },
-	{ title: "Confidentiality Letter", pos: "mid", status: "null" },
-	{ title: "Identification", pos: "mid", status: "null" },
-	{ title: "PCR Result", pos: "right", status: "null" },
+  { title: "Curiculum Vitae", pos: "left", status: "correct" },
+  { title: "Learning Agreement", pos: "mid", status: "correct" },
+  { title: "Invitation Letter", pos: "mid", status: "review" },
+  { title: "Accomodation Letter", pos: "mid", status: "null" },
+  { title: "Arrival Tickets", pos: "mid", status: "null" },
+  { title: "Intern Dev Plan", pos: "mid", status: "incorrect" },
+  { title: "Confidentiality Letter", pos: "mid", status: "null" },
+  { title: "Identification", pos: "mid", status: "null" },
+  { title: "PCR Result", pos: "right", status: "null" },
 ];
 
 const DocumentList = () => {
-	return (
-		<div className="flex flex-col w-full py-2 px-6 gap-2 bg-white border rounded-md">
-			{/* Top */}
-			<div className="flex justify-between">
-				
-				{/* Top Left */}
-				<div className="flex gap-4 items-center">
-					<div className="flex font-semibold">
-						<p>Fabrizio David</p>
-					</div>
+  const [data, setData] = useState([]);
+  const [isloading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/student")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
 
-					<div className="flex items-center gap-1 text-xs font-light text-gray-500">
-						<WorkOutline className="text-sm" />
-						<p>Project Management</p>
-					</div>
+  return (
+    <div className="flex flex-col w-full gap-2">
+      {/* Title Container */}
+      <div className="flex flex-col gap-4 justify-between rounded-t px-4 mb-4 pb-6 border-b-2 border-gray-400">
+        <div className="font-semibold text-2xl">
+          <h3>Applicant Documents</h3>
+        </div>
+        <div className="flex gap-4 text-xs">
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-green-500" />
+              The documents have been received electronically and are correct
+            </div>
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-red-500" />
+              The documents has been reviewed but it is incorrect
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-blue-500" />
+              The documents need to be reviewed
+            </div>
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-gray-400" />
+              The documents has not submitted yet
+            </div>
+          </div>
+        </div>
+      </div>
 
-					<div className="flex items-center gap-1 text-xs font-light text-gray-500">
-						<AssignmentTurnedInOutlined className="text-sm" />
-						Completed
-					</div>
-				</div>
+      {data.map((student) => (
+        <div
+          className="flex flex-col w-full py-2 px-6 gap-2 bg-white border rounded-md"
+          key={student.id}
+        >
+          {/* Top */}
+          <div className="flex justify-between">
+            {/* Top Left */}
+            <div className="flex gap-4 items-center">
+              <div className="flex font-semibold">
+                <p>
+                  {student.firstName} {student.lastName}
+                </p>
+              </div>
+              <div className="flex items-center gap-1 text-xs font-light text-gray-500">
+                <WorkOutline className="text-sm" />
+                <p>{student.applicant.department}</p>
+              </div>
+            </div>
+            {/* Top Right */}
+            <div className="flex gap-2">
+              <div className="py-1 px-2 text-xs rounded bg-sky-200 text-blue-900">
+                Arriving at {student.applicant.arrivalDate}
+              </div>
+              <MoreHoriz className="cursor-pointer" />
+            </div>
+          </div>
 
-				{/* Top Right */}
-				<div className="flex gap-2">
-					<div className="py-1 px-2 text-xs rounded bg-sky-200 text-blue-900">
-						Arriving at 03 September, 2021
-					</div>
-					<MoreHoriz className="cursor-pointer" />
-				</div>
-			</div>
-
-			{/* Middle */}
-			<div className="flex gap-[2px]">
-				{Contents.map((content, index) => (
-					<DocumentListContent
-						key={index}
-						title={content.title}
-						pos={content.pos}
-						status={content.status}
-					/>
-				))}
-			</div>
-
-			{/* Bottom */}
-			<div className="flex justify-between">
-				{/* Bottom Left */}
-				<div className="flex items-center gap-1 text-xs font-light text-gray-500">
-					<p>Applied on 25 August, 2021</p>
-				</div>
-				<div className="flex cursor-pointer">
-					{/* Bottom Right */}
-					<div className="py-1 px-2 text-xs text-blue-900">
-						View All Documents
-					</div>
-					<ArrowRightAlt />
-				</div>
-			</div>
-		</div>
-	);
+          {/* Middle */}
+          <div className="flex gap-[2px]">
+            {Contents.map((content, index) => (
+              <DocumentListContent
+                key={index}
+                title={content.title}
+                pos={content.pos}
+                status={content.status}
+              />
+            ))}
+          </div>
+          {/* Bottom */}
+          <div className="flex justify-between">
+            {/* Bottom Left */}
+            <div className="flex items-center gap-1 text-xs font-light text-gray-500">
+              <p>Applied on {student.applicant.applicationDate}</p>
+            </div>
+            <div className="flex cursor-pointer">
+              {/* Bottom Right */}
+              <div className="py-1 px-2 text-xs text-blue-900">
+                View All Documents
+              </div>
+              <ArrowRightAlt />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default DocumentList;
--- pages/applicants/list.jsx
+++ pages/applicants/list.jsx
@@ -5,164 +5,165 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function ApplicantsList() {
+  const [data, setData] = useState([]);
+  const [isloading, setLoading] = useState(true);
 
-    const [data, setData] = useState([])
-    const [isloading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/student")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+  console.log(data);
+  if (isloading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
 
-    useEffect(() => {
-        setLoading(true)
-        fetch('/api/student')
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data)
-                setLoading(false)
-            })
-    }, [])
-    console.log(data)
-    if (isloading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
-
-    return (
-        <section className="relative w-full">
-            <div className="w-full mb-12">
-                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded">
-                    {/* Title Container */}
-                    <div className="flex justify-between rounded-t mb-0 px-4 py-3 border-0 bg-white">
-                        <div className="flex flex-wrap items-center">
-                            <div className="relative w-full px-4 max-w-full flex-grow flex-1 ">
-                                <h3 className="font-semibold text-2xl">Applicant List</h3>
-                            </div>
-                        </div>
-                        <div className="flex gap-2">
-                            <Link href="/import-list">
-                                <span className="gap-1 hover:bg-gray-200 group flex items-center rounded-md bg-gray-300 text-gray-500 text-xs font-light pl-2 pr-3 py-2 shadow-sm cursor-pointer">
-                                    <SystemUpdateAlt className="text-sm" />
-                                    CSV Import
-                                </span>
-                            </Link>
-                            <Link href="/applicants/new">
-                                <span className="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-xs font-light pl-2 pr-3 py-2 shadow-sm cursor-pointer">
-                                    <Add className="text-sm" />
-                                    Add Candidate
-                                </span>
-                            </Link>
-                        </div>
-                    </div>
-
-                    {/* Table */}
-                    <div className="block w-full overflow-x-auto ">
-                        <table className="items-center w-full border-collapse bg-white">
-                            {/* Table Head */}
-                            <thead>
-                                <tr>
-                                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Full Name
-                                    </th>
-                                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Applied On
-                                    </th>
-                                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Department
-                                    </th>
-                                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Completion{" "}
-                                    </th>
-                                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Status
-                                    </th>
-                                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Action
-                                    </th>
-                                </tr>
-                            </thead>
-
-                            {/* Table Body */}
-                            <tbody className="divide-y">
-                                {data.map(student =>
-                                    <tr key={student.id}>
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                                            <span className="ml-3 font-bold"> {student.firstName} {student.lastName} </span>
-                                        </td>
-
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            {student.applicant.applicationDate}
-                                        </td>
-
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            {student.applicant.department}
-                                        </td>
-
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div className="flex flex-col gap-1">
-                                                <div>CEO Interview</div>
-                                                <div className="flex items-center">
-                                                    <span className="mr-2">60%</span>
-                                                    <div className="relative w-full">
-                                                        <div className="overflow-hidden h-2 text-xs flex rounded bg-gray-300">
-                                                            <div
-                                                                style={{ width: "60%" }}
-                                                                className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
-                                                            ></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                            <div className="flex items-center gap-2">
-                                                <Circle className="h-3 w-3 text-yellow-500" />
-                                                On Progress
-                                            </div>
-                                        </td>
-
-                                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                            <a
-                                                href="#pablo"
-                                                className="text-blueGray-500 block py-1 px-3"
-                                            // onclick="openDropdown(event,'table-dark-1-dropdown')"
-                                            >
-                                                <MoreHoriz />
-                                            </a>
-                                            <div
-                                                className="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
-                                            // id="table-dark-1-dropdown"
-                                            >
-                                                <a
-                                                    href="#pablo"
-                                                    className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-                                                >
-                                                    Action
-                                                </a>
-                                                <a
-                                                    href="#pablo"
-                                                    className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-                                                >
-                                                    Another action
-                                                </a>
-                                                <a
-                                                    href="#pablo"
-                                                    className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-                                                >
-                                                    Something else here
-                                                </a>
-                                                <div className="h-0 my-2 border border-solid border-blueGray-100"></div>
-                                                <a
-                                                    href="#pablo"
-                                                    className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-                                                >
-                                                    Seprated link
-                                                </a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )}
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+  return (
+    <section className="relative w-full">
+      <div className="w-full mb-12">
+        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded">
+          {/* Title Container */}
+          <div className="flex justify-between rounded-t mb-0 px-4 py-3 border-0 bg-white">
+            <div className="flex flex-wrap items-center">
+              <div className="relative w-full px-4 max-w-full flex-grow flex-1 ">
+                <h3 className="font-semibold text-2xl">Applicant List</h3>
+              </div>
             </div>
-        </section>
-    );
+            <div className="flex gap-2">
+              <Link href="/import-list">
+                <span className="gap-1 hover:bg-gray-200 group flex items-center rounded-md bg-gray-300 text-gray-500 text-xs font-light pl-2 pr-3 py-2 shadow-sm cursor-pointer">
+                  <SystemUpdateAlt className="text-sm" />
+                  CSV Import
+                </span>
+              </Link>
+              <Link href="/applicants/new">
+                <span className="hover:bg-green-400 group flex items-center rounded-md bg-green-500 text-white text-xs font-light pl-2 pr-3 py-2 shadow-sm cursor-pointer">
+                  <Add className="text-sm" />
+                  Add Candidate
+                </span>
+              </Link>
+            </div>
+          </div>
+
+          {/* Table */}
+          <div className="block w-full overflow-x-auto ">
+            <table className="items-center w-full border-collapse bg-white">
+              {/* Table Head */}
+              <thead>
+                <tr>
+                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Full Name
+                  </th>
+                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Applied On
+                  </th>
+                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Department
+                  </th>
+                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Completion{" "}
+                  </th>
+                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Status
+                  </th>
+                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+
+              {/* Table Body */}
+              <tbody className="divide-y">
+                {data.map((student) => (
+                  <tr key={student.id}>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                      <span className="ml-3 font-bold">
+                        {" "}
+                        {student.firstName} {student.lastName}{" "}
+                      </span>
+                    </td>
+
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {student.applicant.applicationDate}
+                    </td>
+
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {student.applicant.department}
+                    </td>
+
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      <div className="flex flex-col gap-1">
+                        <div>CEO Interview</div>
+                        <div className="flex items-center">
+                          <span className="mr-2">60%</span>
+                          <div className="relative w-full">
+                            <div className="overflow-hidden h-2 text-xs flex rounded bg-gray-300">
+                              <div
+                                style={{ width: "60%" }}
+                                className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
+                              ></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      <div className="flex items-center gap-2">
+                        <Circle className="h-3 w-3 text-yellow-500" />
+                        On Progress
+                      </div>
+                    </td>
+
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                      <a
+                        href="#pablo"
+                        className="text-blueGray-500 block py-1 px-3"
+                        // onclick="openDropdown(event,'table-dark-1-dropdown')"
+                      >
+                        <MoreHoriz />
+                      </a>
+                      <div
+                        className="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+                        // id="table-dark-1-dropdown"
+                      >
+                        <a
+                          href="#pablo"
+                          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                        >
+                          Action
+                        </a>
+                        <a
+                          href="#pablo"
+                          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                        >
+                          Another action
+                        </a>
+                        <a
+                          href="#pablo"
+                          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                        >
+                          Something else here
+                        </a>
+                        <div className="h-0 my-2 border border-solid border-blueGray-100"></div>
+                        <a
+                          href="#pablo"
+                          className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+                        >
+                          Seprated link
+                        </a>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
