7318b76fc31e8b15a0926d3a398e998591f19432
lia15 <lia@extramus.eu>
2022-09-01 17:27:49 -1200

    final attendance and month attendance

--- pages/interns/monthAttendance.jsx
+++ pages/interns/monthAttendance.jsx
@@ -20,9 +20,9 @@ const attendence = () => {
 								<div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
 									<svg aria-hidden="true" className="w-5 h-5 text-white-500 dark:text-white-400" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path strokeWidth="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
 								</div>
-								<input type="text" id="simple-search" className="rounded border-none bg-[#0B3768]/75 px-10 text-white h-8 placeholder:italic placeholder:text-white/30 placeholder:text-sm" placeholder="Search by name.." required />
+								<input type="text" id="simple-search" className="rounded border-none bg-[#0B3768]/75 px-10 text-white h-10 placeholder:italic placeholder:text-white/30 placeholder:text-sm" placeholder="Search by name.." required />
 							</div>
-							<button type="submit" className="w-10 px-2 rounded border-none bg-blue-100 h-8 ml-1 mr-2 hover:bg-[#0B3768]/75 ">
+							<button type="submit" className="w-10 px-2 rounded border-none bg-blue-100 h-10 ml-1 mr-2 hover:bg-[#0B3768]/75 ">
 								<svg className="w-5 h-5" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
 								<span className="sr-only">Search</span>
 							</button>
