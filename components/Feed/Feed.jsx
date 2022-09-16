2a8f932c338777aaf726c7c3612bec7d9dd0a4db
sinemturkcu <sinemturkcu@gmail.com>
2022-09-16 18:17:21 +0200

    The problem was solved

--- components/Feed/Feed.jsx
+++ components/Feed/Feed.jsx
@@ -1,32 +1,39 @@
+import { useEffect, useState } from "react";
 const Feed = () => {
-	return (
-		<div className="flex m-2 py-4">
-			<div className="flex flex-[1] flex-col gap-2 p-2">
-				<div className="text-sm font-semibold">20 August 2021</div>
-				<div className="text-xs font-light">
-					<div>posted by</div>
-					<div>Antonio Gallo</div>
-				</div>
-			</div>
-			<div className="flex flex-[3] flex-col gap-2 p-2">
-				<div className="text-sm font-semibold">
-					Project Management Presentation
-				</div>
-				<div className="text-xs font-light">
-					In monday 23 august there Will he a presentation from project
-					management regardina the theater platform. Please prepare the
-					necessary documents. In monday 23 august there Will he a presentation
-					from project management regardina the theater platform. Please prepare
-					the necessary documents.
-				</div>
-			</div>
-			<div className="flex flex-[1] p-2">
-				<div className="flex h-fit text-sm font-semibold underline cursor-pointer">
-					Read More
-				</div>
-			</div>
-		</div>
-	);
+  const [data, setData] = useState([]);
+  const [isloading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/whatsNew")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  return (
+    <div>
+      {data.slice(data.length - 3).map((whatsNew) => (
+        <div className="flex m-2 py-4" key={whatsNew.id}>
+          <div className="flex flex-[1] flex-col gap-2 p-2">
+            <div className="text-sm font-semibold">{whatsNew.date}</div>
+            <div className="text-xs font-light">
+              <div>posted by</div>
+              <div>{whatsNew.postedBy}</div>
+            </div>
+          </div>
+          <div className="flex flex-[3] flex-col gap-2 p-2">
+            <div className="text-sm font-semibold">{whatsNew.title}</div>
+            <div className="text-xs font-light">{whatsNew.title}</div>
+          </div>
+          <div className="flex flex-[1] p-2">
+            <div className="flex h-fit text-sm font-semibold underline cursor-pointer">
+              Read More
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Feed;
