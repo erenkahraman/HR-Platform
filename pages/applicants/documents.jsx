80b99deca62377ee14d528b3eb3d7ceaa3b5e4f8
sinemturkcu <sinemturkcu@gmail.com>
2022-09-22 11:02:16 +0200

    Applicant Document List added

--- pages/applicants/documents.jsx
+++ pages/applicants/documents.jsx
@@ -1,58 +1,38 @@
 import { Circle } from "@mui/icons-material";
-import { DocumentList } from "../../components/DocumentList";
-import {
-  ArrowRightAlt,
-  AssignmentTurnedInOutlined,
-  CheckCircleOutline,
-  MoreHoriz,
-  WorkOutline,
-} from "@mui/icons-material";
-import { useEffect, useState } from "react";
+import { ApplicantDocumentList } from "../../components/ApplicantDocumentList";
 
 export default function Documents() {
-  const Contents = [
-    { title: "Curiculum Vitae", pos: "left", status: "correct" },
-    { title: "Learning Agreement", pos: "mid", status: "correct" },
-    { title: "Invitation Letter", pos: "mid", status: "review" },
-    { title: "Accomodation Letter", pos: "mid", status: "null" },
-    { title: "Arrival Tickets", pos: "mid", status: "null" },
-    { title: "Intern Dev Plan", pos: "mid", status: "incorrect" },
-    { title: "Confidentiality Letter", pos: "mid", status: "null" },
-    { title: "Identification", pos: "mid", status: "null" },
-    { title: "PCR Result", pos: "right", status: "null" },
-  ];
-  const DocumentListContent = ({ title, pos, status }) => {
-    const Border = () => {
-      let isRounded;
-      let statusColor;
-
-      pos === "left"
-        ? (isRounded = " rounded-l-md ")
-        : pos === "right"
-        ? (isRounded = " rounded-r-md ")
-        : isRounded;
-
-      status === "correct"
-        ? (statusColor = " bg-green-400 ")
-        : status === "incorrect"
-        ? (statusColor = " bg-red-400 ")
-        : status === "review"
-        ? (statusColor = " bg-blue-400 ")
-        : (statusColor = " bg-gray-400 ");
-
-      let result =
-        "flex flex-col items-center px-2 py-1 w-full gap-1 text-white " +
-        isRounded +
-        statusColor;
-      return result;
-    };
-
-    return (
-      <div className={Border()}>
-        <div className="text-[10px] ">{title}</div>
-        <CheckCircleOutline className="text-sm" />
+  return (
+    <div className="flex flex-col w-full gap-2">
+      {/* Title Container */}
+      <div className="flex flex-col gap-4 justify-between rounded-t px-4 mb-4 pb-6 border-b-2 border-gray-400">
+        <div className="font-semibold text-2xl">
+          <h3>Applicant Documents</h3>
+        </div>
+        <div className="flex gap-4 text-xs">
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-green-500" />
+              The documents have been received electronically and are correct
+            </div>
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-red-500" />
+              The documents has been reviewed but it is incorrect
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-blue-500" />
+              The documents need to be reviewed
+            </div>
+            <div className="flex gap-2 items-center">
+              <Circle className="text-xs text-gray-400" />
+              The documents has not submitted yet
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  };
-  return <DocumentList />;
+      <ApplicantDocumentList />
+    </div>
+  );
 }
