3fb58dbd2ce413adc3736727974b7d1598581190
sinemturkcu <sinemturkcu@gmail.com>
2022-09-22 09:09:50 +0200

    I pushed because of fetching error

--- models/intern.js
+++ models/intern.js
@@ -11,6 +11,15 @@ const InternSchema = new mongoose.Schema({
   departement: { type: String, maxlength: 30 },
   position: { type: String, maxlength: 30 },
   student: { type: mongoose.Schema.Types.ObjectId, ref: "Student" },
+  attendance: {
+    date: { type: String, default: 0 },
+    present: { type: String, default: 0 },
+    dayOff: { type: String, default: 0 },
+    late: { type: String, default: 0 },
+    excusedLeave: { type: String, default: 0 },
+    sick: { type: String, default: 0 },
+    unexcusedleave: { type: String, default: 0 },
+  },
 });
 
 export default mongoose.models.Intern || mongoose.model("Intern", InternSchema);
