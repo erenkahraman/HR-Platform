b27639bd6f081bb24a96a053493a6e0c4b977a62
yigitcankkeskin <yigitcan@extramus.eu>
2022-10-07 19:26:36 +0300

    part of the authentication process done

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,33 @@
 import { useRouter } from "next/router";
 import { Layout } from "../components/Layout";
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
+import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-	const router = useRouter();
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  const router = useRouter();
 
-	if (router.asPath === "/login" || router.asPath === "/login/forgot"  ) {
-		return <Component {...pageProps} />;
-	} else {
-		return (
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		);
-	}
+  if (router.asPath === "/login" || router.asPath === "/login/forgot") {
+    return (
+      <SessionProvider session={session}>
+        <Component {...pageProps} />{" "}
+      </SessionProvider>
+    );
+  } else {
+    return (
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    );
+  }
 }
 
 export default MyApp;
