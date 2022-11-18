ba3fde0c533500d680750a25068b7be58ff96b89
Abuerx <anouar@extramus.com>
2022-11-18 09:09:58 +0300

    deleted duplicated file on department api

--- pages/api/department/[field].js
+++ pages/api/department/[field].js
@@ -1,13 +1,28 @@
 import dbConnect from "../../../util/mongodb";
 import Department from "../../../models/department";
+import { tokenCheckFunction } from "../auth/tokenCheck";
 
 export default async function handler(req, res) {
   await dbConnect();
 
   const {
-    query: { field },
+    query: { id },
     method,
   } = req;
+
+  // Token CHECK
+  let token = req.query.token
+    ? req.query.token
+    : req.body.token
+    ? req.body.token
+    : "";
+  try {
+    tokenCheckFunction(token);
+  } catch (e) {
+    console.error(e);
+    res.status(401).json("Unauthorized User");
+  }
+  // Token CHECK
 
   switch (method) {
     case "GET":
