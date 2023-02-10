5b3ff109501a724bb9930fe75adfd143af303e68
Toto2444 <79368546+Toto2444@users.noreply.github.com>
2023-02-10 12:34:22 +0100

    Final version send email

--- models/email.js
+++ models/email.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 
 const EmailSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
     from : { type: String },
     to : { type: String },
-    message : { type: String }
+    message : { type: String },
+    date : {type: Date }
 });
 
 export default mongoose.models.Email || mongoose.model('Email', EmailSchema);
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,8 +1,11 @@
 import nodemailer from "nodemailer";
+import cookie from "js-cookie";
+
 
 export default async (req, res) => {
-  const { name, email, message } = req.body;
+  const {emailFrom, emailTo, subject, message } = req.body;
 
+ 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -14,18 +17,18 @@ export default async (req, res) => {
     }
   });
 
-  try {
-    await transporter.sendMail({
-      from: process.env.GOOGLE_USER,
-      to: email,
-      subject: `Contact form submission from ${name}`,
-      html: `<p>You have a contact form submission</p><br>
-        <p><strong>Email: </strong> ${email}</p><br>
-        <p><strong>Message: </strong> ${message}</p><br>
-      `
-    });
-  } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
-  }
-  return res.status(200).json({ error: "" });
+  
+// verify connection configuration
+
+try {
+  await transporter.sendMail({
+    from:  process.env.GOOGLE_USER, //emailFrom
+    to: emailTo,
+    subject: subject,
+    html: `<p>${message}</p>`
+  });
+} catch (error) {
+  return res.status(500).json({ error: error.message || error.toString() });
+}
+return res.status(200).json({ error: "" });
 };
