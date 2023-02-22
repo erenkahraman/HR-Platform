1613a51d3bd54c7192b0bdd741b664a35bbeeac0
Alwast <storeyalbertwm@gmail.com>
2023-02-22 11:29:53 +0100

    Final view of reports

--- pages/dashboard/reports.jsx
+++ pages/dashboard/reports.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from "react";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import TrendingDownIcon from "@mui/icons-material/TrendingDown";
 import StackedBarChartOutlinedIcon from "@mui/icons-material/StackedBarChartOutlined";
-import { Box, Button, Card, CardBody, Grid, Paper, Typography } from "@mui/material";
+import { Box, Button, Card, Grid, Paper, Typography } from "@mui/material";
 import { CardFooter } from "@material-tailwind/react";
 import IconButton from "@mui/material/IconButton";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import CloseIcon from "@mui/icons-material/Close";
 import Slide from "@mui/material/Slide";
-import { CreateChart1, CreateBarChart, CreatePieChart } from "../../components/Charts/allCharts";
+import {
+  CreateChart1,
+  CreateBarChart,
+  CreatePieChart,
+} from "../../components/Charts/allCharts";
 
 const Statistics = {
   heading: "Statistical Report",
