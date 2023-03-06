f3488cf6be2f8b8e16f207a1946d6198c5c5cb1f
Toto2444 <79368546+Toto2444@users.noreply.github.com>
2023-03-06 14:33:10 +0100

    add upload document intern part

---
+++ .history/components/MultiStepForm/MultiStepForm_20230302164712.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useForm, useStep } from "react-hooks-helper";
+import { Names } from "./stepForm/Names";
+import { Address } from "./stepForm/Address";
+import { Contact } from "./stepForm/Contact";
+import { Review } from "./stepForm/Review";
+import { Submit } from "./stepForm/Submit";
+const MultiStepForm = () => {
+    const []
+};
---
+++ .history/pages/applicants/newMultiStep_20230302163600.jsx
@@ -0,0 +1,8 @@
+import { useState, useMemo, useEffect } from "react";
+import MultiStepForm from "../../components/MultiStepForm/MultiStepForm";
+
+export default function ApplicantsNew() {
+	return (
+
+    );
+}
