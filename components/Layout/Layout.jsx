c4bba3c7ffd64f6b1a7eda7e39ba652e7d4bb608
Yumit Demir <yumitdemir@outlook.com>
2023-06-05 18:52:21 +0200

    Change layout display from flex to grid to fix the  bugs and make it responsive.

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -21,15 +21,15 @@ export default function Layout({children}) {
                     <title>HR Project</title>
                 </Head>
                 {/* Main Container */}
-
-                    <div className={`flex flex-col flex-1 lg:flex-row w-full  min-w-0 `}>
+                {/*flex flex-col flex-1 lg:flex-row w-full flex-wrap  gap-10 min-w-0*/}
+                    <div className={`grid grid-cols-1 lg:grid-cols-7  gap-10`}>
                         {/* SideNavbar */}
-                        <div className={` ${open? "" : "hidden" }`}>
+                        <div className={`col-span-7 lg:col-span-1 ${open? "" : "hidden" }`}>
                             <Sidebar/>
                         </div>
 
                         {/* Right Side */}
-                        <div className={""}>{children}</div>
+                        <div className={`w-full   ${open? "col-span-7 lg:col-span-6" : "col-span-7 lg:col-span-7" }`}>{children}</div>
                     </div>
 
 
