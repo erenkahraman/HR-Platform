e5bf33e1d85d74b67d1c28b7074f8c3c08546a7f
Kutay <kutay.alptekin@std.yildiz.edu.tr>
2023-07-12 13:45:34 +0200

    import react to newMultiStep

--- components/MultiStepForm/StepForm/AppDetails.jsx
+++ components/MultiStepForm/StepForm/AppDetails.jsx
@@ -33,7 +33,6 @@ export const AppDetails = ({ formData, setForm, navigation }) => {
             )}
           />
             </LocalizationProvider>*/}
-       
         </FormControl>
     </Container>
   );
--- pages/applicants/newMultiStep.jsx
+++ pages/applicants/newMultiStep.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import MultiStepForm from "../../components/MultiStepForm/MultiStepForm";
 
 export default function ApplicantsNew() {
