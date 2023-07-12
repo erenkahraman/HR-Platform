8a020b587de165ff9a316c1e2a4bef0a4993a4e7
Kutay <kutay.alptekin@std.yildiz.edu.tr>
2023-07-12 13:54:28 +0200

    newMultiStep import changes

--- pages/applicants/newMultiStep.jsx
+++ pages/applicants/newMultiStep.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect } from "react";
-import MultiStepForm from "../../components/MultiStepForm/MultiStepForm";
+import MultiStepForm from "../../components/MultiStepForm";
 
 export default function ApplicantsNew() {
 	return (
