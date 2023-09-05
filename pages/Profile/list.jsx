84ce78dda4790f15afcfc86b98f6918677bfbdca
Desi168 <84246146+Desi168@users.noreply.github.com>
2023-09-05 12:36:39 +0300

    Community page - added Document section page

--- pages/Profile/list.jsx
+++ pages/Profile/list.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { Tooltip } from "@material-tailwind/react";
 import { AiOutlineEdit } from "react-icons/ai";
+// import { IoDocumentOutline } from "react-icons/ai";
+import { FaRegFile } from 'react-icons/fa';
 import axios from "axios";
 import cookie from "js-cookie";
 import LoadingState from "../../components/Utils/LoadingState.jsx";
@@ -29,6 +31,7 @@ const profileListTableHeaders = [
   "Department / Position",
   "Start Date",
   "End Date",
+  "Document",
   "Action",
 ];
 
@@ -318,6 +321,25 @@ export default function ApplicantsList({ status }) {
 
                       <TableCell>{student.applicant.endDate}</TableCell>
 
+                      <TableCell>            
+                           <Tooltip
+                          className="bg-transparent text-black mt-2"
+                          content="Document"
+                          animate={{
+                            mount: { scale: 1, y: 0 },
+                            unmount: { scale: 0, y: 25 },
+                          }}
+                        >
+                          <Button
+                            variant="gradient"
+                            className="text-black bg-transparent scale-100 hover:scale-125 p-0 cursor-pointer text-xl"
+                          >                          
+                            <Link href="/applicants/allDocuments">                    
+                                <FaRegFile/>
+                            </Link>
+                          </Button>
+                        </Tooltip></TableCell>
+
                       <TableCell>
                         <Tooltip
                           className="bg-transparent text-black mt-2"
@@ -331,7 +353,13 @@ export default function ApplicantsList({ status }) {
                             variant="gradient"
                             className="text-black bg-transparent scale-100 hover:scale-125 p-0 cursor-pointer text-xl"
                           >
-                            <Link href="/applicants/edit">
+                            {/* <Link href="/applicants/edit"> */}
+                            <Link
+                              href={{
+                                pathname: "/applicants/new",
+                                query: { student: JSON.stringify(student) },
+                              }}
+                              as={`/interns/${student.firstName}_${student.lastName}`}>
                               <AiOutlineEdit className="m-2 mb-0 mt-0" />
                             </Link>
                           </Button>
