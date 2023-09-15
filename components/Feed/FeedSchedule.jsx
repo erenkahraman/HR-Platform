f39e42463b9fa3d22bd503a5d1f34fdd34c53492
aivalfmioc <flaviamioc@yahoo.com>
2023-09-15 17:59:26 +0200

    fixing the weekly schedule from the dashboard

--- components/Feed/FeedSchedule.jsx
+++ components/Feed/FeedSchedule.jsx
@@ -6,17 +6,24 @@ import axios from "axios";
 import cookie from "js-cookie";
 
 const FeedSchedule = () => {
+  const WEEKDAYS = 5;
   const [weeklySchedule, setWeeklySchedule] = useState([]);
   const [departmentNames, setDepartmentNames] = useState([]);
   const [morningDepartments, setMorningDepartments] = useState([]);
   const [afternoonDepartments, setAfternoonDepartments] = useState([]);
 
+  const [dateRange, setDateRange] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [weeklyScheduleByDepartment, setWeeklyScheduleByDepartment] = useState({});
+  const [populatedWeeklySchedule, setPopulatedWeeklySchedule] = useState([]);
   const token = cookie.get("token");
   // console.log("received cookies from FeedSchedule");
 
   useEffect(() => {
     const asyncRequest = async () => {
       try {
+
+        handleCurrentWeekDateRange();
         const config = {
           headers: {
             "Content-Type": "application/json",
@@ -27,31 +34,112 @@ const FeedSchedule = () => {
           { params: { token: token } },
           config
         );
+        const { weeklySchedule, populatedWeeklySchedule } = data;
 
-        const weeklyScheduleGroupedByDepartment = data.reduce(
-          (departments, item) => {
-            const department = departments[item.department] || [];
-            department.push(item);
-            departments[item.department] = department;
-            return departments;
-          },
-          {}
-        );
-        setWeeklySchedule(weeklyScheduleGroupedByDepartment);
+        // Update state with the new data
+        setWeeklySchedule(weeklySchedule);
+        setPopulatedWeeklySchedule(populatedWeeklySchedule);
 
-        const departmentNames = Object.keys(weeklyScheduleGroupedByDepartment);
-        setDepartmentNames(departmentNames);
 
-        const halfLength = Math.ceil(departmentNames.length / 2);
-        setMorningDepartments(departmentNames.slice(0, halfLength));
-        setAfternoonDepartments(departmentNames.slice(halfLength));
+        // const weeklyScheduleGroupedByDepartment = data.reduce(
+        //   (departments, item) => {
+        //     const department = departments[item.department] || [];
+        //     department.push(item);
+        //     departments[item.department] = department;
+        //     return departments;
+        //   },
+        //   {}
+        // );
+        // setWeeklySchedule(weeklyScheduleGroupedByDepartment);
+
+        setPopulatedWeeklySchedule(populatedWeeklySchedule);
+        // const departmentNames = Object.keys(weeklyScheduleGroupedByDepartment);
+        // setDepartmentNames(departmentNames);
+
+        // const halfLength = Math.ceil(departmentNames.length / 2);
+        // setMorningDepartments(departmentNames.slice(0, halfLength));
+        // setAfternoonDepartments(departmentNames.slice(halfLength));
       } catch (e) {
         console.error(e);
       }
     };
     asyncRequest();
   }, [token]);
+  useEffect(() => {
+    handleCurrentWeekDateRange();
+    const fetchInterns = async () => {
+      try {
 
+        const token = cookie.get("token");
+        if (!token) {
+          console.log("Token Expired! error function: fetchInterns");
+          return;
+        }
+        else {
+          console.log("Token value from fetchInterns", token);
+        }
+
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          params: {
+            token: token,
+          },
+        };
+      }
+      catch (e) {
+        console.error(e);
+      }
+    }
+  })
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (anchorEl && !event.target.closest(`#department-menu-${selectedDepartment}`)) {
+        setAnchorEl(null);
+        setSelectedDepartment(null);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+  const formatDate = (date) => {
+    const yyyy = date.getFullYear();
+    let mm = date.getMonth() + 1;
+    let dd = date.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const formattedDay = dd + '/' + mm + '/' + yyyy;
+
+    return formattedDay
+  }
+
+  const addDays = (date, days) => {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  const substractDays = (date, days) => {
+    const result = new Date(date);
+    result.setDate(result.getDate() - days);
+    return result;
+  }
+
+  const handleCurrentWeekDateRange = () => {
+    const currentDate = new Date();
+    const todayNameIndex = currentDate.getDay();
+    const firstDayOfTheWeek = substractDays(currentDate, todayNameIndex - 1);
+    const formattedFirstDayOfTheWeek = formatDate(firstDayOfTheWeek);
+    const lastDayOfTheWeek = addDays(currentDate, WEEKDAYS - todayNameIndex);
+    const formattedLastDayOfTheWeek = formatDate(lastDayOfTheWeek);
+    const weekDateRange = `${formattedFirstDayOfTheWeek} - ${formattedLastDayOfTheWeek}`;
+    setDateRange(weekDateRange);
+  }
 
   const read = () => {
     confirmAlert({
@@ -83,11 +171,9 @@ const FeedSchedule = () => {
       buttons: [
         {
           label: 'OK',
-          onClick: () => alert('Click Yes')
         },
         {
           label: 'Cancel',
-          onClick: () => alert('Click No')
         }
       ]
     });
@@ -96,10 +182,9 @@ const FeedSchedule = () => {
   return (
     <div className="flex flex-wrap m-2 py-4">
       <div className="flex flex-[1] flex-col gap-2 p-2">
-        <div className="text-sm font-semibold">10 August 2022</div>
+        <div className="text-sm font-semibold">  {dateRange}</div>
         <div className="text-xs font-light">
-          <div>posted by</div>
-          <div>Antonio Gallo</div>
+
         </div>
       </div>
       <div className="flex flex-[3]  flex-col  ">
@@ -108,22 +193,30 @@ const FeedSchedule = () => {
           <div className="flex flex-wrap justify-center flex gap-8 my-1">
             <div>
               <h3>Morning Shift:</h3>
-              {departmentNames.map((eachDepartmentName) => (
-                <div key={eachDepartmentName}>
-                  <br />
-                  <br />
-                  {eachDepartmentName + ": " + weeklySchedule[eachDepartmentName].length}
-                </div>
-              ))}
+              <div>
+                {populatedWeeklySchedule.map((schedule) => (
+                  <table
+                    key={schedule._id}            >
+                    <thead>
+                      <tr>
+                        <h3><strong>{schedule.Group} <span>[{schedule.Schedule.morning.length}]</span> </strong></h3>
+                      </tr>
+                    </thead>
+                  </table>
+
+                ))}
+              </div>
             </div>
             <div>
               <h3>Afternoon Shift:</h3>
-              {departmentNames.map((eachDepartmentName) => (
-                <div key={eachDepartmentName}>
-                  <br />
-                  <br />
-                  {eachDepartmentName + ": " + weeklySchedule[eachDepartmentName].length}
-                </div>
+
+              {populatedWeeklySchedule.map((schedule) => (
+                <table
+                  key={schedule._id}
+                >
+
+                  <h3><strong>{schedule.Group}<span>[{schedule.Schedule.afternoon.length}]</span> </strong></h3>
+                </table>
               ))}
             </div>
           </div>
@@ -131,7 +224,7 @@ const FeedSchedule = () => {
       </div>
       <button onClick={read} className="flex flex-[1] p-2">
         <div className="flex h-fit text-sm font-semibold underline cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-300 ...">
-          Read More
+
         </div>
       </button>
     </div>
